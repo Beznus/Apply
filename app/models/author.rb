2743c001dd8cde609ea4bde6a311c0dd7a8b1954
Stephen Cassidy <cassidy.1113@gmail.com>
2015-12-11 22:20:19 -0500

    add import entries and authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+
+  def self.import_author(author_data)
+    Author.find_or_create_by(name: author_data.css("name").inner_text).id 
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,13 @@
 class Entry < ActiveRecord::Base
+
+  def self.import_entries(file)
+    file.css("entry").each do |entry_data|
+      entry = Entry.find_or_create_by(posting_id: entry_data.css("id").inner_text)
+      entry.update_attributes(published: entry_data.css("published").inner_text.to_datetime, 
+         link: entry_data.css("link").attribute('href').value,
+         title: entry_data.css("title").inner_text,
+         content: entry_data.css("content").inner_text,
+         author_id: Author.import_author(entry_data.css("author")))
+    end
+  end
 end
